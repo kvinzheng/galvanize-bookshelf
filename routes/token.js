e1d997d2d9bde110b9281bcf0b703712acb09e41
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-03-12 13:07:29 -0700

    ifinished part 4

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,65 @@
 'use strict';
-
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex.js');
+const humps = require('humps');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const env = process.env.NODE_ENV || 'developments';
 
-// YOUR CODE HERE
+router.get('/token', (req, res) => {
+    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+        if (err) {
+            res.send(false);
+        } else {
+            res.send(true);
+        }
+    });
+});
+
+router.post('/token', (req, res, next) => {
+    return knex('users').where('email', req.body.email).returning('*').first()
+        .then((user) => {
+            if ((user === undefined)) {
+                res.set('Content-Type', 'text/plain');
+                res.status(400).send('Bad email or password');
+            } else {
+                bcrypt.compare(req.body.password, user.hashed_password)
+                    .then((result) => {
+                        if (result === false) {
+                            res.set('Content-Type', 'text/plain');
+                            res.status(400).send('Bad email or password');
+                        }
+
+                        const claim = {
+                            userId: user.id
+                        };
+                        const token = jwt.sign(claim, process.env.JWT_KEY, {
+                            expiresIn: '7 days'
+                        });
+                        res.cookie('token', token, {
+                            httpOnly: true,
+                            expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7 days
+                            secure: router.get('env') === 'production' // Set from the NODE_ENV
+                        });
+
+                        res.set('Content-Type', 'application/json');
+                        console.log('log 2', user);
+                        delete user.hashed_password;
+                        res.send(humps.camelizeKeys(user));
+                    });
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
+
+router.delete('/token', (req, res, next) => {
+    res.clearCookie('token');
+    res.send(true);
+});
+
 
 module.exports = router;
