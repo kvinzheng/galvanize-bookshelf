088a6160a8b63a2fed89d027a12f28c55a2fbb75
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-03-12 13:26:09 -0700

    updated eed file and migration file

--- migrations/20170312131827_favorites.js
+++ migrations/20170312131827_favorites.js
@@ -1,11 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('favorites', (table) =>{
+  return knex.schema.createTable('favorites', (table) => {
     table.increments().primary();
-    
+    table.integer('book_id').onDelete('CASCADE').notNullable();
+    table.integer('user_id').onDelete('CASCADE').notNullable();
+    table.timestamp(true, true);
   })
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('favorites');
 };
